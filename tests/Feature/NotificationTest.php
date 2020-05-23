d3bf2c888c1e85ee9131b91a14f55285883484a9
exatasmente <lu_u_z@hotmail.com>
2020-05-23 15:19:12 -0300

    remoção de imports não usados

--- tests/Feature/NotificationTest.php
+++ tests/Feature/NotificationTest.php
@@ -4,12 +4,10 @@ namespace Tests\Feature;
 
 use App\Models\NotificacaoDeAtendimento;
 use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class NotificationTest extends TestCase
+class TestNotification extends TestCase
 {
     use RefreshDatabase;
 
