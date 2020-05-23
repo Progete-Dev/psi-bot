da85b5abaf9e25a48acb357a31addeb273caf6d0
exatasmente <lu_u_z@hotmail.com>
2020-05-23 15:15:43 -0300

    troca do nome classe notification test

--- tests/Feature/NotificationTest.php
+++ tests/Feature/NotificationTest.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class TestNotification extends TestCase
+class NotificationTest extends TestCase
 {
     use RefreshDatabase;
 
