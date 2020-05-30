6a9f17f1f41145e3e0ef000a15678d09080feced
Leticia Freitas <leticiafventura@alu.ufc.br>
2020-05-30 18:01:09 -0300

    atualização historico

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -2,13 +2,13 @@
 <div class="container my-4">
     <div class="bg-white shadow overflow-hidden  rounded">
         @if(isset($cardTitle))
-            <div class="px-4 py-5 border-b border-gray-200 sm:px-6">
+            <div class="px-4 py-5 border-b border-gray-200 sm:px-6 flex">
                 <h3 class="text-lg leading-6 font-medium text-indigo-600">
                 {{ $cardTitle ?? ''}}
                 </h3>
-                <p class="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
-                {{-- Personal details and application. --}}
-                </p>
+                @if(isset($cardHeader))
+                    {{$cardHeader}}
+                @endif
             </div>
         @endif
         <div class="px-4 py-5 sm:px-6">
