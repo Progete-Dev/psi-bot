67ff2260d8d10a49d59d03571e137badca8b4868
exatasmente <lu_u_z@hotmail.com>
2020-06-13 15:37:08 -0300

    driver

--- routes/botman.php
+++ routes/botman.php
@@ -1,16 +1,18 @@
 <?php
 use App\Http\Controllers\BotManController;
 use App\Http\Middleware\LoginMiddleware;
-
-
+use BotMan\Drivers\Whatsappgo\WhatsappgoDriver;
 
 $botman = resolve('botman');
+
 // $botman->middleware->received(new LoginMiddleware());
 // // $botman->group(['middleware' => new LoginMiddleware()], function($bot) {
     
     
 // // });
-$botman->hears("", BotManController::class."@startConversation");
+$botman->group(['driver' => WhatsappgoDriver::class],function($bot){
+    $bot->hears("", BotManController::class."@startConversation");
+});
 //$botman->hears("test", BotManController::class."@test");
 
 
