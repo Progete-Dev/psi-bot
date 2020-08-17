f2e50213e111f22d24b58c2e021b530c5a0db63b
Luiz Neto <lu_u_z@hotmail.com>
2020-08-17 15:15:20 -0300

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(FormularioCadastroSeeder::class);
-        $this->call(AtendimentoTableSeeder::class);
+        //$this->call(AtendimentoTableSeeder::class);
     }
 }
